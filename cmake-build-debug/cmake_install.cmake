48f66e682b9feda0d6b15469cdfd0eceafda3d5b
Thulio Silva <thuliosilva13@outlook.com>
2022-06-14 10:46:34 -0300

    initial commit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/thuli/Documents/TP-AEDS-II
+# Install script for directory: C:/Users/thuli/OneDrive/Área de Trabalho/TrabalhoPraticoAEDS2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/thuli/Documents/TP-AEDS-II/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/thuli/OneDrive/Área de Trabalho/TrabalhoPraticoAEDS2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
